47dbb98c765b4a835adc1956926d4a05b39f3e84
justdoit712 <lc204573@gmail.com>
2026-05-19 09:05:00 +0800

    feat(userpay): 支持手动输入客户ID并校验充值

--- beacon-webmaster/src/main/resources/static/public/js/client/userpay.js
+++ beacon-webmaster/src/main/resources/static/public/js/client/userpay.js
@@ -9,14 +9,20 @@ var vm = new Vue({
     el: '#dtapp',
     data: {
         clientId: '',
+        clientIdError: '',
         corpname: '',
         currentMoney: null,
+        clientVerified: false,
+        looking: false,
         amount: '',
         amountError: '',
         submitting: false
     },
     computed: {
         currentMoneyDisplay: function () {
+            if (!this.clientVerified) {
+                return '请先查询客户';
+            }
             if (this.currentMoney === null || this.currentMoney === undefined) {
                 return '加载中...';
             }
@@ -25,29 +31,55 @@ var vm = new Vue({
     },
     created: function () {
         var id = getUrlParam('clientId');
-        if (!id) {
-            layer.alert('缺少客户参数，请从客户接入列表进入', function () {
-                vm.goBack();
-            });
-            return;
+        if (id) {
+            this.clientId = id;
+            this.lookupClient();
         }
-        this.clientId = id;
-        this.loadClientInfo(id);
     },
     methods: {
-        loadClientInfo: function (id) {
+        lookupClient: function () {
+            var id = $.trim(this.clientId);
+            if (!id) {
+                this.clientIdError = '请输入客户业务 ID';
+                return;
+            }
+            if (!/^\d+$/.test(id)) {
+                this.clientIdError = '客户 ID 必须为数字';
+                return;
+            }
+            this.clientIdError = '';
+            this.clientVerified = false;
+            this.corpname = '';
+            this.currentMoney = null;
+            this.looking = true;
+
             $.get('../sys/client-business/info/' + id, function (r) {
+                vm.looking = false;
                 if (r.code === 0 && r.data) {
                     var info = r.data.clientbusiness || r.data;
+                    if (!info.corpname && !info.id) {
+                        vm.clientIdError = '该客户 ID 不存在，请核实后重试';
+                        vm.clientVerified = false;
+                        return;
+                    }
                     vm.corpname = info.corpname || '';
                     vm.currentMoney = info.money != null ? info.money : 0;
+                    vm.clientVerified = true;
+                    vm.clientIdError = '';
                 } else {
-                    vm.corpname = '未知客户';
-                    vm.currentMoney = 0;
+                    vm.clientIdError = (r.msg || '客户不存在') + '，请核实后重试';
+                    vm.clientVerified = false;
                 }
+            }).fail(function () {
+                vm.looking = false;
+                vm.clientIdError = '查询失败，请检查网络后重试';
             });
         },
         validate: function () {
+            if (!this.clientVerified) {
+                this.clientIdError = '请先查询并确认客户信息';
+                return false;
+            }
             var val = parseInt(this.amount, 10);
             if (!this.amount || isNaN(val)) {
                 this.amountError = '请输入充值金额';
